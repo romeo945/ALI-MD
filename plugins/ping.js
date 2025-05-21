57f8139ba6946436fa4163082d7a372fdd73c02b
romeo945 <itzromeoroy@gmail.com>
2025-05-21 13:15:21 +0530

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -130,7 +130,7 @@ cmd({
 async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
     try {
         const startTime = Date.now()
-        const message = await conn.sendMessage(from, { text: '\`ALI PINGING 🚀\`' })
+        const message = await conn.sendMessage(from, { text: '\`PONG🍉\`' })
         const endTime = Date.now()
         const ping = endTime - startTime
         await conn.sendMessage(from, { text: `*ALI PONGED ! : ${ping}ms ⚡*` }, { quoted: message })
