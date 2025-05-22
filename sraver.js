c1512721a94250198c521845d506aa230ec9db8b
romeo945 <itzromeoroy@gmail.com>
2025-05-22 13:17:11 +0530

    Create sraver.js

---
+++ sraver.js
@@ -0,0 +1,12 @@
+import {app} from "./app.js";
+app.get("/", (req, res) => {
+ res.send("<h1>Working Fine</h1>");
+});
+app.route("/users").get((req, res, next) => {
+  res.status(200).json({
+    users: [],
+    success: false,
+  });
+});
+
+app.listen(process.env.PORT, () => {
