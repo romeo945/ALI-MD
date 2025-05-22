9260a86ef6dfb0794105a9ad2b47600762357d6a
romeo945 <itzromeoroy@gmail.com>
2025-05-22 13:23:18 +0530

    Update and rename sraver.js to server.js

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-import {app} from "./app.js";
+import {app} from "./index.js";
+
 app.get("/", (req, res) => {
  res.send("<h1>Working Fine</h1>");
 });
+
 app.route("/users").get((req, res, next) => {
   res.status(200).json({
     users: [],
@@ -10,3 +12,5 @@ app.route("/users").get((req, res, next) => {
 });
 
 app.listen(process.env.PORT, () => {
+   console.log(Server is working on Port ${process.env.PORT});
+});
